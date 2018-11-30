161de627ad625178134fb49d38c2396b7127c7a8
metalwarrior665 <lukaskrivka@gmail.com>
2018-11-30 15:32:42 +0100

    added path to images with default

--- src/default-functions.js
+++ src/default-functions.js
@@ -5,18 +5,21 @@ module.exports.defaultFileNameFunction = (url) => {
 };
 
 module.exports.defaultPostDownloadFunction = (data, imagesObject, fileNameFunction) => {
+    console.log(Object.keys(imagesObject).length)
     return data.map((item) => {
         const newImages = item.images.map((imageUrl) => {
-            if (imagesObject[imageUrl].imageUploaded){
+            if (imagesObject[imageUrl] && imagesObject[imageUrl].imageUploaded){
                 return{
                     sourceUrl: imageUrl,
                     fileUrl: fileNameFunction(imageUrl),
                 }
-            } else {
+            } else if (imagesObject[imageUrl]) {
                 return{
                     sourceUrl: imageUrl,
                     errors: imagesObject[imageUrl].errors,
                 }
+            } else {
+                return imageUrl
             }
         });
         return { ...item, images: newImages };
