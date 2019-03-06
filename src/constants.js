c18bdc4aad571adfe468a52930dd7e89544315bb
metalwarrior665 <lukaskrivka@gmail.com>
2019-03-06 17:49:05 +0100

    changed batch size to 10000

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,5 @@ module.exports = {
     PROXY_URL: Apify.getApifyProxyUrl(),
     REQUEST_INTERNAL_TIMEOUT: 6000,
     REQUEST_EXTERNAL_TIMEOUT: 7000,
-    DATASET_BATCH_SIZE: 500,
+    DATASET_BATCH_SIZE: 10000,
 };
